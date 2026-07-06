9e0fec2742dd6fe32baabce8e05a747915d66b7f
rauniyar-aman <198869061+rauniyar-aman@users.noreply.github.com>
2026-07-06 14:11:23 +0545

    Remove unused files, design mockups and default Vite assets

--- frontend/src/components/common/AuthCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AuthCard({ children }) {
-  return (
-    <div className="mx-auto w-full max-w-md rounded-[32px] border border-surface-container bg-surface p-8 shadow-[0_30px_60px_-30px_rgba(15,23,42,0.2)]">
-      {children}
-    </div>
-  )
-}
